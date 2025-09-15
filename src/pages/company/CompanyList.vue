8eb26a3ef2968299af8ee94d5fdda275d73aa907
Shobhin Shaji <shobhin9747@gmail.com>
2025-09-15 06:14:10 +0100

    design changes

--- src/pages/company/CompanyList.vue
+++ src/pages/company/CompanyList.vue
@@ -17,40 +17,40 @@
                     <table class="w-full">
                         <thead class="border-b border-[#F4F4F4]">
                             <tr>
-                                <th class="px-6 py-3 text-left text-base font-medium text-[#022652] ">
+                                <th class="px-6 py-4 text-left text-base font-medium text-[#022652] ">
                                     Name</th>
-                                <th class="px-6 py-3 text-left text-base font-medium text-[#022652]">
+                                <th class="px-6 py-4 text-left text-base font-medium text-[#022652]">
                                     Company Number</th>
-                                <th class="px-6 py-3 text-left text-base font-medium text-[#022652]">
+                                <th class="px-6 py-4 text-left text-base font-medium text-[#022652]">
                                     Industry</th>
-                                <th class="px-6 py-3 text-left text-base font-medium text-[#022652]">
+                                <th class="px-6 py-4 text-left text-base font-medium text-[#022652]">
                                     Jurisdiction</th>
-                                <th class="px-6 py-3 text-center text-base font-medium text-[#022652]">
+                                <th class="px-6 py-4 text-center text-base font-medium text-[#022652]">
                                     Employee Count</th>
-                                <th class="px-6 py-3 text-left text-base font-medium text-[#022652]">
+                                <th class="px-6 py-4 text-left text-base font-medium text-[#022652]">
                                     Status</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr v-for="(company, index) in companies" :key="index" 
                                 :class="['hover:bg-gray-50', index < companies.length - 1 ? 'border-b border-[#F4F4F4]' : '']">
-                                <td class="px-6 py-4 whitespace-nowrap">
+                                <td class="px-6 py-5 whitespace-nowrap">
                                     <div class="flex items-center gap-1">
                                         <CompanyIcon />
                                         <div class="text-sm font-medium text-[#333333]">{{ company.name }}</div>
                                     </div>
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-[#333333]">{{ company.companyNumber
+                                <td class="px-6 py-5 whitespace-nowrap text-sm text-[#333333]">{{ company.companyNumber
                                     }}</td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-[#333333]">{{ company.industry }}
+                                <td class="px-6 py-5 whitespace-nowrap text-sm text-[#333333]">{{ company.industry }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-[#333333]">{{ company.jurisdiction
+                                <td class="px-6 py-5 whitespace-nowrap text-sm text-[#333333]">{{ company.jurisdiction
                                 }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-center text-sm text-[#333333]">{{
+                                <td class="px-6 py-5 whitespace-nowrap text-center text-sm text-[#333333]">{{
                                     company.employeeCount
                                 }}</td>
-                                <td class="px-6 py-4 whitespace-nowrap">
+                                <td class="px-6 py-5 whitespace-nowrap">
                                     <span
                                         class="inline-flex px-2 py-1 text-xs font-semibold rounded-full text-[#5EBA4F]">
                                         {{ company.status }}
